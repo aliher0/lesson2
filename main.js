92342da01d7d918215a1d42e2e1951211c6fbc01
aliher0 <alisertolgoev434@gmail.com>
2022-11-07 15:37:14 +0300

    lesson2

---
+++ main.js
@@ -0,0 +1,39 @@
+// var my_name = prompt ("Атынызды жазыныз !")
+// console.log("HELLO" + my_name)v
+//Тип данных doolen
+// 0-false 1 -true
+
+
+
+// Тип данных undefined
+
+// var myName;
+// console.log( myName + 1) //undefined
+
+//  Тип  данных  null
+
+
+
+
+
+// Тип данных object-объект
+var namy = prompt("АТЫНЫЗДЫ ЖАЗЫНЫЗ.!")
+console.log(namy)
+console.log('салам' + namy)
+var obj = {
+    name: 'aliher',
+    surname:'tolgoev',
+    age:13,
+    married:false,
+    judging:null,
+    info:undefined,
+    pasportData:{
+        id: 'AN564738434537',
+        series: 13354747487,
+        organ:'MKK-547'
+    }
+
+}
+
+console.log()
+console.log(obj)
